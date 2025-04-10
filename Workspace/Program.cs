7cb9d3cc8bdccb961c19b15d325c485155fa35e2
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-04-10 20:34:50 +0900

    Add files.

---
+++ Workspace/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EmdFlat;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+}
---
+++ clean.bat
@@ -0,0 +1,5 @@
+rmdir /s /q "EmdFlat\bin"
+rmdir /s /q "EmdFlat\obj"
+rmdir /s /q "Workspace\bin"
+rmdir /s /q "Workspace\obj"
+pause
