b40e3b0729d97b76dc3f1a17629fd7d9b1be0efe
Nobuaki Tanaka <sinshu.d@gmail.com>
2025-04-10 22:10:05 +0900

    IT WORKS!!!!!!!!!!

--- EmdFlat/signature_t.cs
+++ EmdFlat/signature_t.cs
@@ -7,5 +7,12 @@ namespace EmdFlat
         public int n;               /* Number of features in the signature */
         public feature_t[] Features;  /* Pointer to the features vector */
         public float[] Weights;     /* Pointer to the weights of the features */
+
+        public signature_t(int n, feature_t[] Features, float[] Weights)
+        {
+            this.n = n;
+            this.Features = Features;
+            this.Weights = Weights;
+        }
     }
 }
--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Numerics;
 using EmdFlat;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static unsafe void Main(string[] args)
     {
-        Console.WriteLine("hello");
+        Vector3[] f1 =
+        [
+            new(100, 40, 22),
+            new(211, 20, 2),
+            new(32, 190, 150),
+            new(2, 100, 100),
+        ];
+        Vector3[] f2 =
+        [
+            new(0, 0, 0),
+            new(50, 100, 80),
+            new(255, 255, 255),
+        ];
+
+        float[] w1 = [0.4F, 0.3F, 0.2F, 0.1F];
+        float[] w2 = [0.5F, 0.3F, 0.2F];
+
+        var s1 = new signature_t<Vector3>(4, f1, w1);
+        var s2 = new signature_t<Vector3>(3, f2, w2);
+
+        var emd = new Emd();
+        var value = emd.emd(s1, s2, (x, y) => (x - y).Length(), null, null);
+        Console.WriteLine(value);
     }
 }
